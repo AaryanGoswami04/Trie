f4cecc6aa6b4a728639cb239d3432cd0be8baaa4
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-11-26 18:40:10 +0530

    Update and rename main (16).cpp to Insertion.cpp

--- Insertion.cpp
+++ Insertion.cpp
@@ -3,10 +3,10 @@ using namespace std;
 class TrieNode{
   public:
     char data;
-    TrieNode* children[26];
-    bool isTerminal;
+    TrieNode* children[26]; //array of pointers to next level trie nodes
+    bool isTerminal;  //To mark end of word node
     TrieNode(char ch){
-      data=ch;
+      data = ch;
       for(int i=0; i<26; i++)
         children[i]=NULL;
       isTerminal=false;
@@ -15,20 +15,22 @@ class TrieNode{
 class Trie{
   public:
     TrieNode* root;
+  
     Trie(){
       root=new TrieNode('\0');     
     }
+  
     void insertuntil(TrieNode* root, string word){
       //base case
-      if(word.length()==0){
-        root->isTerminal=true;
+      if(word.length() == 0){
+        root->isTerminal = true; //Mark the node as end of the word
         return;
       }
       int index = word[0]-'A';
       TrieNode* child;
       
-      if(root->children[index]!=NULL)
-        child=root->children[index];
+      if(root->children[index] != NULL)  //Key is present
+        child = root->children[index];
         
       else{  //absent
         child=new TrieNode(word[0]);
@@ -37,6 +39,7 @@ class Trie{
       //recursion
       insertuntil(child, word.substr(1));
     }
+  
     void insertword(string word){
       insertuntil(root, word);
     }
